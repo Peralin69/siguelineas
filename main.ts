ccd3527c8f9aa9793649b10066950c59d68a705d
Peralin69 <99762461+Peralin69@users.noreply.github.com>
2022-03-30 18:32:10 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,11 +4,17 @@ basic.forever(function () {
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 25)
     }
     if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 10)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 15)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 25)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
     }
     if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 10)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 15)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 25)
     }
     while (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
